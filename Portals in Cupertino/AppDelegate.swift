228aa4bd8234590db4dc15c15f5546b482e1efa1
Marquis Kurt <software@marquiskurt.net>
2019-10-09 16:18:22 -0400

    Initial Commit

---
+++ Portals in Cupertino/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Portals in Cupertino
+//
+//  Created by Marquis Kurt on 10/9/19.
+//  Copyright © 2019 Marquis Kurt. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
