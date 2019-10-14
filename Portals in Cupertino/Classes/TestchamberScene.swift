fa289259b01d05aacd95b17e2d2022fb964096fb
Marquis Kurt <software@marquiskurt.net>
2019-10-13 23:19:47 -0400

    [Classes + Testchambers] Update MyScene to SampleTestchamber and hook up tile map assignments

--- Portals in Cupertino/Classes/TestchamberScene.swift
+++ Portals in Cupertino/Classes/TestchamberScene.swift
@@ -10,13 +10,14 @@ import Foundation
 import SpriteKit
 import GameplayKit
 
-class TestchamberScene: GameScene {
+class TestchamberScene: SKScene {
     
     // MARK: Attributes
     
     var exitDoor: SKSpriteNode?
     var walls: [SKSpriteNode]?
     var playerNode: SKSpriteNode?
+    var inputs: [SKSpriteNode]?
     
     // MARK: Tile Map Configurations
     
@@ -32,14 +33,16 @@ class TestchamberScene: GameScene {
         
         // Create some constants to keep track of things
         let tileMapSize = map.tileSize
-        let halfWidth = tileMapSize.width / 2
-        let halfHeight = tileMapSize.height / 2
+        let halfWidth = CGFloat(map.numberOfColumns) / 2.0 * tileMapSize.width
+        let halfHeight = CGFloat(map.numberOfRows) / 2.0 * tileMapSize.height
         let tileMapPosition = map.position
         
         // Create the appropriate door textures (used to track the type of obstructions) and player texture
         let metalDoorTexture = SKTexture(imageNamed: "Metal_Wall_With_Door")
         let concreteDoorTexture = SKTexture(imageNamed: "Concrete_Wall_with_Door")
         let playerTexture = SKTexture(imageNamed: "Player")
+        let metalButtonTexture = SKTexture(imageNamed: "Metal_Button")
+        let concreteButtonTexture = SKTexture(imageNamed: "Concrete_Button")
         
         // Iterate over every item in the tile map
         for y in 0 ..< map.numberOfColumns {
@@ -52,14 +55,19 @@ class TestchamberScene: GameScene {
                     let firstTexture = tileTextures[0]
                     
                     // Calculate the tile's position
-                    let nodeX = CGFloat(y) * tileMapSize.width - halfWidth + (halfWidth)
-                    let nodeY = CGFloat(x) * tileMapSize.height - halfHeight + (halfHeight)
+                    let nodeX = CGFloat(y) * tileMapSize.width - halfWidth + (tileMapSize.width / 2)
+                    let nodeY = CGFloat(x) * tileMapSize.height - halfHeight + (tileMapSize.height / 2)
                     
                     // Create a new node with the tile's texture
                     let newTileNode = SKSpriteNode(texture: firstTexture)
+                    newTileNode.isHidden = false
                     
                     // Place the node at the calculated position
                     newTileNode.position = CGPoint(x: nodeX, y: nodeY)
+                    newTileNode.zPosition = 1
+                    
+                    newTileNode.lightingBitMask = 0b0001
+                    newTileNode.shadowCastBitMask = 0b0001
                     
                     // Assign a physics body to the node and change its properties.
                     newTileNode.physicsBody = SKPhysicsBody(texture: firstTexture,
@@ -72,9 +80,6 @@ class TestchamberScene: GameScene {
                     newTileNode.physicsBody?.allowsRotation = false // Doesn't rotate
                     newTileNode.physicsBody?.friction = 0.7
                     
-                    // Add the node as a child
-                    self.addChild(newTileNode)
-                    
                     // Check the tile node's texture. If it's a player or door, treat it differently.
                     if (newTileNode.texture != nil) {
                         switch (newTileNode.texture) {
@@ -84,6 +89,15 @@ class TestchamberScene: GameScene {
                             break;
                         case (concreteDoorTexture):
                             self.exitDoor = newTileNode
+                            break;
+                        // Buttons: Assign them as inputs
+                        case (metalButtonTexture):
+                            newTileNode.zPosition = 0
+                            self.inputs?.append(newTileNode)
+                            break;
+                        case (concreteButtonTexture):
+                            newTileNode.zPosition = 0
+                            self.inputs?.append(newTileNode)
                             break;
                         // Player: Assign it as the player
                         case (playerTexture):
@@ -97,6 +111,9 @@ class TestchamberScene: GameScene {
                         }
                     }
                     
+                    // Add the node as a child
+                    self.addChild(newTileNode)
+                    
                     // Fix the position again
                     newTileNode.position = CGPoint(x: newTileNode.position.x + tileMapPosition.x,
                                                    y: newTileNode.position.y + tileMapPosition.y)
@@ -104,5 +121,17 @@ class TestchamberScene: GameScene {
                 
             }
         }
+        
+        // Remove the tilemap
+        map.removeFromParent()
+    }
+    
+    // MARK: Overrides
+    
+    override func didMove(to view: SKView) {
+        guard let roomLayout = childNode(withName: "roomLayout") as? SKTileMapNode else {
+            fatalError("Room layout is missing. Aborting...")
+        }
+        self.configureLayoutFromTilemap(roomLayout)
     }
 }
