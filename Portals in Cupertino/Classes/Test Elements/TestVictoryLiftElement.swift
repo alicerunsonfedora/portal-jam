a77902bddd3abbefedfcb4b0d5d2d5ed1c27bbe5
Marquis Kurt <software@marquiskurt.net>
2019-10-21 18:02:36 -0400

    [Classes/Test Elements] Animate the player before transport, add docs

--- Portals in Cupertino/Classes/Test Elements/TestVictoryLiftElement.swift
+++ Portals in Cupertino/Classes/Test Elements/TestVictoryLiftElement.swift
@@ -9,24 +9,52 @@
 import Foundation
 import SpriteKit
 
+/**
+ A victory lift.
+ 
+ Victory lifts typically transport players to the next available testchamber as described in `transportLocation`. These should be used only a segues to other SpriteKit scenes.
+ */
 class TestVictoryLiftElement {
     
+    /**
+     The name of the scene to transport to
+     */
     var transportLocation: String
+    
+    /**
+     The view to present the transporting scene with
+     */
     var view: SKView?
+    
+    /**
+     The associated `SKSpriteNode`
+     */
     var elementNode: SKSpriteNode
     
+    /**
+     Check to see if the player has stepped on the victory lift.
+     
+     If the player is on the victory lift, an animation  will play where the player realigns themself on the victory lift and then the new scene will be presented.
+     */
     func watchForActivation() {
         let playerMap = self.elementNode.parent?.children.filter({ $0 is Player }).map({ node in node as! Player })
         let player: Player? = playerMap?[0]
         
         if player != nil {
             if player!.isCloseTo(node: self.elementNode) {
-                self.transportToLocation()
+                player!.run(SKAction.move(to: self.elementNode.position, duration: 0.75)) {
+                    player!.run(SKAction.rotate(toAngle: (3 * .pi) / 2, duration: 1.5)) {
+                        self.transportToLocation()
+                    }
+                }
             }
         }
         
     }
     
+    /**
+     Present the new scene, if possible.
+     */
     func transportToLocation() {
         let transportScene = SKScene(fileNamed: transportLocation)
                 
@@ -37,6 +65,13 @@ class TestVictoryLiftElement {
         }
     }
     
+    /**
+     Initialize a victory lift.
+     - Parameters:
+        - toLocation: The SKScene name to transport to
+        - node: The node to associate the victory lift with
+        - view: The `SKView` to control for presentation
+     */
     init(toLocation: String, node: SKSpriteNode, view: SKView?) {
         self.transportLocation = toLocation
         self.elementNode = node
